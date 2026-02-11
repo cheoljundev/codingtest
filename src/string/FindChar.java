2579c0c0af38d69a84066d7800f3666bcb7e3a57
cheoljundev <cheoljundev@gmail.com>
2026-02-11 21:18:41 +0900

    문자 빈도수 찾기 - 내 풀이

---
+++ src/string/FindChar.java
@@ -0,0 +1,29 @@
+package string;
+
+import java.util.Scanner;
+
+/**
+ * @author cheoljundev
+ * @extends
+ * @implements
+ * @since 2026. 2. 11.
+ */
+public class FindChar {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.nextLine().toLowerCase();
+        String findChar = sc.nextLine().toLowerCase();
+
+        int result = 0;
+        for (int i = 0; i < string.length(); i++) {
+
+            char c = string.charAt(i);
+            if (c == findChar.charAt(0)) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
